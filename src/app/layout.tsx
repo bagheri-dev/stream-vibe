3fd8a238ff4dcd9830a52274684c9581892f4996
Mahdi Bagheri <bagheri.develop@gmail.com>
2025-05-11 22:57:27 +0330

    start project

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "StreomVibe",
+  description: "Streom Vibe move app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
