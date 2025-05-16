9d49823de192526be5236c9883622880fd5698bb
Mahdi Bagheri <bagheri.develop@gmail.com>
2025-05-16 21:06:42 +0330

    responsive banner and features

--- src/components/shared/Banner.tsx
+++ src/components/shared/Banner.tsx
@@ -1,21 +1,25 @@
-import {SiteContainer} from "@/lib/SiteContainer";
+import { SiteContainer } from "@/lib/SiteContainer";
 
 export const Banner = () => {
     return (
         <div className={"pb-16 lg:pb-[116px]"}>
             <SiteContainer>
-                <div className={"flex items-center justify-between bg-[url(/images/banner.webp)] bg-center bg-cover py-[100px] px-20 rounded-[20px]"}>
-                    <div>
-                        <h2 className={"text-5xl pb-[14px] text-white font-manrope-bold"}>Start your free trial today!</h2>
-                        <p className={"text-lg grey60"}>This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.</p>
+                <div className={"flex flex-col lg:flex-row items-center justify-between bg-[url(/images/banner.webp)] bg-center bg-cover py-12 lg:py-[100px] px-6 lg:px-20 rounded-[20px]"}>
+                    <div className="text-center lg:text-left mb-6 lg:mb-0">
+                        <h2 className={"text-3xl lg:text-5xl pb-[14px] text-white font-manrope-bold"}>
+                            Start your free trial today!
+                        </h2>
+                        <p className={"text-lg text-gray-300"}>
+                            This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.
+                        </p>
                     </div>
-                    <div>
+                    <div className="text-center lg:text-left">
                         <button className={"cursor-pointer py-4 px-6 bg-red45 text-white text-lg font-manrope-semibold rounded-xl"}>
-                            Start a Free Trail
+                            Start a Free Trial
                         </button>
                     </div>
                 </div>
             </SiteContainer>
         </div>
-    )
+    );
 }
